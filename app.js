2d2eb05bf967047df24d3e3e761da98b0cadf119
Casweeney Ojukwu <casweeno2000@gmail.com>
2021-05-30 13:18:05 +0100

    Adding tasks to task list

--- app.js
+++ app.js
@@ -2,3 +2,43 @@
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+//load all event listeners
+loadEventListeners();
+
+//load all event listeners
+function loadEventListeners(){
+    //Add task event
+    form.addEventListener('submit', addTask);
+}
+
+//Add task
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Add a task');
+    }
+
+    //Create li element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    //create text node and append to the li
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    //create new link element
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></a>';
+
+    //append link to li
+    li.appendChild(link);
+
+    //append li to ul
+    taskList.appendChild(li);
+
+    //clear input
+    taskInput.value = '';
+
+    e.preventDefault();
+}
